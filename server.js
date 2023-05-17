f5d034c2d47055515142be629f72d535dc935742
Colin Cason <48043196+colinjcason@users.noreply.github.com>
2023-05-17 09:57:19 -0700

    added new API key

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(cors())
 require('dotenv').config()
 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY
 
 app.post('/completions', async (req, res) => {
   const options = {
